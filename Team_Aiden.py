73c56913a61fd9bf6540b729e1b0d1eb088d2a1c
Aiden Lee <216723@sps.org>
2019-01-28 16:17:37 +0000

    Test

---
+++ Team_Aiden.py
@@ -0,0 +1,22 @@
+team_name = 'Team_Aiden'
+strategy_name = 'Collude until betrayed'
+strategy_description = '''\
+Collude first round. Collude, unless betrayed; then retaliate. Resume collusion.'''
+    
+def move(my_history, their_history, my_score, their_score):
+    '''Make my move based on the history with this player.
+    
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
+    '''
+    if 'b' in their_history:
+        if their_history[-1] == 'c' and my_history[-1] == 'b':
+            return 'b'
+        else:
+            return 'c'
+    else:
+        return 'c'
