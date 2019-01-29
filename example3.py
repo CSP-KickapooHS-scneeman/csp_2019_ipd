4221649c21b6a25a9d1ee9007a0e4411de83cbec
scneeman <30477959+scneeman@users.noreply.github.com>
2019-01-29 10:41:42 -0600

    Update example3.py

--- example3.py
+++ example3.py
@@ -22,9 +22,10 @@ a severe punishment.'''
     
     Returns 'c' or 'b' for collude or betray.
     '''
-if len(my_history)==0: # It's the first round; collude.
-    return 'c'
-elif my_history[-1]=='c' and their_history[-1]=='b':
-    return 'b' # Betray if they were severely punished last time,
-else:
-    return 'c' # otherwise collude.
+def move(my_history, their_history, my_score, their_score):
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1]=='c' and their_history[-1]=='b':
+        return 'b' # Betray if they were severely punished last time,
+    else:
+        return 'c' # otherwise collude.
